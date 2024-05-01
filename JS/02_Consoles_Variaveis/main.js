22f6b2b7fe04cfa19bfb10b8561e82d12d10dff5
joaovictorfariaspessoa <joaovictorfp08@gmail.com>
2024-04-30 21:28:37 -0300

    separando a pasta

--- JS/02_Consoles_Variaveis/main.js
+++ JS/02_Consoles_Variaveis/main.js
@@ -33,3 +33,18 @@ console.clear();
 const pessoa = 'Irmão do Jorel';
 const idade2 = 30;
 console.log('Meu nome é ' + pessoa + ' e eu tenho ' + idade2 + ' anos!');
+
+// Template String
+const hello = `Meu nome é ${pessoa} e eu tenho ${idade2} anos!`;
+console.log(hello);
+
+// Operadores de comparação e lógico
+console.clear();
+let teste1 = 1;
+let teste2 = 0;
+let valor1 = true;
+let valor2 = false;
+console.log(teste1 > teste2); // Operador de comparação
+console.log(teste1 < teste2); // Operador de comparação
+console.log(teste1 && teste2); // Operador lógico
+console.log(valor1 && valor2); // Operador lógico
