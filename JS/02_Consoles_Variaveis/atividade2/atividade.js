ef796d9e7f7c58b3b343c7cae9c9b6076dba3458
joaovictorfariaspessoa <joaovictorfp08@gmail.com>
2024-05-02 21:42:37 -0300

    Aula de java

---
+++ JS/02_Consoles_Variaveis/atividade2/atividade.js
@@ -0,0 +1,16 @@
+// String, Numbers, Boolean, null, undefined
+console.clear();
+const nome_pessoa = 'John';
+const idade1 = 30;
+const rating = 4.5;
+const isCool = true;
+const x = null;
+const y = undefined;
+let z;
+console.log(typeof nome_pessoa); // Type of const
+console.log(typeof idade1);
+console.log(typeof rating);
+console.log(typeof isCool);
+console.log(typeof x);
+console.log(typeof y);
+console.log(typeof z);
---
+++ JS/03_strings/atividade2/atividade.js
@@ -0,0 +1,10 @@
+// Métodos de strings
+console.clear();
+const string01 = 'Instituto da Oportunidade Social';
+
+// Maiusculo e minúsculo
+console.clear();
+const s = 'Abacate';
+console.log(s.toUpperCase());
+console.log(s.toLowerCase()); 
+
