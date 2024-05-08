19be74115f1b5f9d1e5624d9b1d9233b4c1eb66d
joaovictorfariaspessoa <joaovictorfp08@gmail.com>
2024-05-08 19:27:52 -0300

    Aula de Laços de Repetição

---
+++ JS/04_Laços_De_Repetição/js/main.js
@@ -0,0 +1,33 @@
+// while
+let contador = 1;
+while (contador <= 10) {
+console.log(`Valor = ${contador}`);
+contador++;
+}
+
+// while com decremento
+console.clear();
+let cont = 10;
+while (cont >= 0) {
+console.log(`Valor = ${cont}`);
+cont--;
+}
+
+// do-while
+console.clear();
+let i = 0,
+text = '';
+do {
+text += `O número é ${i}\n`;
+i++;
+} while (i < 10);
+console.log(text);
+
+// For
+console.clear();
+for (let i = 0; i < 10; i++) console.log(`Laço For número: ${i}`);
+
+// For - Decremento
+console.clear();
+for (let i = 10; i > 0; i--) console.log(`Laço For número: ${i}`);
+
