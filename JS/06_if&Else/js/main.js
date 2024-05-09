efdafc65d1de475cf8ceed9c116a384b602c0530
joaovictorfariaspessoa <joaovictorfp08@gmail.com>
2024-05-08 21:49:46 -0300

    Final da Aula

---
+++ JS/06_if&Else/js/main.js
@@ -0,0 +1,30 @@
+console.clear();
+const num = 'Olá';
+const g = 11;
+
+if(num % 2==0 && !isNaN(num))
+console.log ('Número par');
+
+else if(num % 2!=0 && !isNaN(num))
+console.log('Número ímpar')
+else console.log('Não é um número')
+
+console.clear();
+let resultado=3>4 ? 'Sim':'Não';
+console.log (resultado)
+
+const num1 new Array(30,-1,5,3,121);
+const mum2 = (-2,40,16,111,33,164);
+
+console.log(num1);
+console.log(num2);
+
+//acessando elementos diversos do array:
+console.log('primeiro elemento de num1: ${num1[0]'});
+console.log('primeiro elemento de num2: ${num2[0]'});
+    
+
+console.log('primeiro elemento de num1: ${num1[2]'});
+console.log('primeiro elemento de num2: ${num2[5]'});
+
+
