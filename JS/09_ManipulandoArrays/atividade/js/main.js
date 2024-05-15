e81bd3a04c4b2f41afd7dfadf92bf51fc7b0f46e
joaovictorfariaspessoa <joaovictorfp08@gmail.com>
2024-05-14 21:40:53 -0300

    Final da Aula

---
+++ JS/09_ManipulandoArrays/atividade/js/main.js
@@ -0,0 +1,19 @@
+// Objetos
+let pessoa = {
+    firstName: 'João Victor',
+    lastName: 'Farias Pessoa',
+    corDosOlhos: 'preto',
+    hobbies: ['música', 'filmes', 'esportes'],
+    endereco: {
+    rua: 'Rua do bobos',
+    numero: 0,
+    estado: 'SP',
+},
+};
+console.log(pessoa);
+console.log(pessoa.firstName);
+console.log(pessoa.lastName, pessoa.idade);
+console.log(pessoa.hobbies[1]);
+console.log(pessoa.endereco.cidade);
+
+
