343d85c6c2c823267200115c2cf2261513260f03
joaovictorfariaspessoa <joaovictorfp08@gmail.com>
2024-05-20 21:08:03 -0300

    Atividade de Padding/Marging

---
+++ JS/12Manipulando_DOM_aula2/js/main.js
@@ -0,0 +1,24 @@
+// Método getElementById
+let titulo = document.getElementById('titulo');
+// Alterando o conteúdo do elemento
+titulo.innerHTML = 'Olá meus caros alunos!';
+// Configurando o estilo CSS do elemento
+titulo.style.textAlign = 'center';
+titulo.style.backgroundColor = '#CCCCC9';
+titulo.style.borderBottom = 'solid 3px #000';
+titulo.style.margin = '20px';
+
+// Método getElementsByClassName
+let items = document.getElementsByClassName('item');
+console.log(items);
+console.log(items[1]);
+items[1].textContent = 'Hello 2';
+items[1].style.fontWeight = 'bold';
+items[1].style.backgroundColor = 'yellow';
+
+for (let i = 0; i < items.length; i++) {
+    items[i].style.backgroundColor = '#f4f4f4';
+    }
+
+
+
