555721d0c71115d91272de893bf3af93e3f434c7
João Victor Farias Pessoa <Arthurmurilocorreiabarros17@gmail.com>
2024-06-28 20:55:47 -0300

    atividade de Laços de Repetição

---
+++ JS/05_Laços_De_Repetição/atividade/main.js
@@ -0,0 +1,21 @@
+// Exercício 1: Mostrar no console os números de 1 ao 50. (while)
+console.log("Exercício 1:");
+let a = 1;
+while (a <= 50) {
+    console.log(a);
+    a++;
+}
+
+// Exercício 2: Mostrar no console contagem regressiva de 10 até 0. (do-while)
+console.log("\nExercício 2:");
+let b = 10;
+do {
+    console.log(b);
+    b--;
+} while (b >= 0);
+
+// Exercício 3: Mostrar no console os números pares de 1 a 100. (for)
+console.log("\nExercício 3:");
+for (let c = 2; c <= 100; c += 2) {
+    console.log(c);
+}
